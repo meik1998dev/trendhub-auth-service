01aa91e82d1114c7d2fee51443a19d88c4c8aa26
meik1998news <mohammad+test@newsifier.com>
2023-08-22 20:25:39 +0300

    feat: Add UserModule and import User entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { User } from './entities/user.entity';
       entities: [User],
       synchronize: true,
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
