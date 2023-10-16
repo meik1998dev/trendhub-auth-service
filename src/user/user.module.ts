2f53b6caf546f1d2ae036464ee9ec86031b92e7f
meik1998news <mohammad+test@newsifier.com>
2023-10-16 21:15:47 +0300

    update user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { JwtStrategy } from 'src/jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [UserService],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'yourSecretKey',
+      signOptions: {
+        expiresIn: '24h',
+      },
+    }),
+  ],
+  providers: [UserService, JwtStrategy],
   controllers: [UserController],
 })
 export class UserModule {}
