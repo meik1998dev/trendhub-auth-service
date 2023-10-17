d936278f58ba8cacd6d6a4cccc25bcb04aaf08ac
meik1998news <mohammad+test@newsifier.com>
2023-10-17 09:55:43 +0300

    create dtos

---
+++ src/user/dto/user.dto.ts
@@ -0,0 +1,13 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  password: string;
+}
+
+export class LoginUserDto extends CreateUserDto {
+  id: number;
+}
